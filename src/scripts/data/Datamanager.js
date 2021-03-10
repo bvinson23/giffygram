df7fa6869389fabbdcb890d0ac79f73e283a355f
bvinson23 <brandonvinson23@gmail.com>
2021-03-10 09:24:57 -0600

    fixed datamanager.js to properly use the eventlistener for change on the year in the footer

--- src/scripts/data/Datamanager.js
+++ src/scripts/data/Datamanager.js
@@ -2,7 +2,15 @@ export const getUsers = () => {
     return fetch("http://localhost:8088/users")
     .then(response => response.json())
 }
+let postCollection = [];
 
+export const usePostCollection = () => {
+    // Best practice: we don't want to alter the original state, so
+    // make a copy of it and then return it
+    // The spread operator makes this quick work
+    let postCollectionCopy = [...postCollection]
+    return postCollectionCopy;
+}
 export const getPosts = () => {
     return fetch("http://localhost:8088/posts")
     .then(response => response.json())
@@ -11,15 +19,7 @@ export const getPosts = () => {
         return parsedResponse;
     })
 }
-let postCollection = [];
 
-export const usePostCollection = () => {
-    // Best practice: we don't want to alter the original state, so
-    // make a copy of it and then return it
-    // The spread operator makes this quick work
-    postCollectionCopy = [...postData]
-    return postCollectionCopy;
-}
 
 const loggedInUser = {
     id: 1,
