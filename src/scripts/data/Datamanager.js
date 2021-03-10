35977c0e225ce6241169b00339df5033487569ed
bvinson23 <brandonvinson23@gmail.com>
2021-03-10 11:05:23 -0600

    removed an unneeded varialbe in main.js.11

--- src/scripts/data/Datamanager.js
+++ src/scripts/data/Datamanager.js
@@ -8,8 +8,7 @@ export const usePostCollection = () => {
     // Best practice: we don't want to alter the original state, so
     // make a copy of it and then return it
     // The spread operator makes this quick work
-    let postCollectionCopy = [...postCollection]
-    return postCollectionCopy;
+     return [...postCollection]
 }
 export const getPosts = () => {
     return fetch("http://localhost:8088/posts")
