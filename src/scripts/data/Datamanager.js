b1d76c4ecb534e8551d71aa06db0ed4080b4a193
bvinson23 <brandonvinson23@gmail.com>
2021-03-24 13:47:32 -0500

    added funcitonality where only the logged in user can edit or delete their own posts

--- src/scripts/data/Datamanager.js
+++ src/scripts/data/Datamanager.js
@@ -1,6 +1,6 @@
 export const getUsers = () => {
     return fetch("http://localhost:8088/users")
-    .then(response => response.json())
+        .then(response => response.json())
 }
 let postCollection = [];
 
@@ -8,19 +8,19 @@ export const usePostCollection = () => {
     // Best practice: we don't want to alter the original state, so
     // make a copy of it and then return it
     // The spread operator makes this quick work
-     return [...postCollection]
+    return [...postCollection]
 }
 
 export const getPosts = () => {
     const userId = getLoggedInUser().id
     return fetch(`http://localhost:8088/posts?_expand=user`)
-      .then(response => response.json())
-      .then(parsedResponse => {
-        console.log("data with user", parsedResponse)
-        postCollection = parsedResponse
-        return parsedResponse;
-      })
-  }
+        .then(response => response.json())
+        .then(parsedResponse => {
+            console.log("data with user", parsedResponse)
+            postCollection = parsedResponse
+            return parsedResponse;
+        })
+}
 
 export const createPost = postObj => {
     return fetch("http://localhost:8088/posts", {
@@ -29,10 +29,10 @@ export const createPost = postObj => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(postObj)
-  
+
     })
         .then(response => response.json())
-  }
+}
 
 let loggedInUser = {}
 
@@ -41,7 +41,7 @@ export const logoutUser = () => {
 }
 
 export const getLoggedInUser = () => {
-	return {...loggedInUser};
+    return { ...loggedInUser };
 }
 
 export const deletePost = postId => {
@@ -50,60 +50,62 @@ export const deletePost = postId => {
         headers: {
             "Content-Type": "application/json"
         }
-  
+
     })
         .then(response => response.json())
         .then(getPosts)
-  }
+}
 
-  export const getSinglePost = (postId) => {
-      return fetch(`http://localhost:8088/posts/${postId}`)
-      .then(response => response.json())
-  }
+export const getSinglePost = (postId) => {
+    return fetch(`http://localhost:8088/posts/${postId}`)
+        .then(response => response.json())
+}
 
-  export const updatePost = postObj => {
-      return fetch(`http://localhost:8088/posts/${postObj.id}`, {
-          method: "PUT",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(postObj)
-      })
-      .then(response => response.json())
-      .then(getPosts)
-  }
-
-  export const setLoggedInUser = (userObj) => {
-      loggedInUser = userObj
-  }
-
-  export const loginUser = (userObj) => {
-    return fetch(`http://localhost:8088/users?name=${userObj.name}&email=${userObj.email}`)
-    .then(response => response.json())
-    .then(parsedUser => {
-      //is there a user?
-      console.log("parsedUser", parsedUser) //data is returned as an array
-      if (parsedUser.length > 0){
-        setLoggedInUser(parsedUser[0]);
-        return getLoggedInUser();
-      }else {
-        //no user
-        return false;
-      }
+export const updatePost = postObj => {
+    return fetch(`http://localhost:8088/posts/${postObj.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postObj)
     })
-  }
+        .then(response => response.json())
+        .then(getPosts)
+}
 
-  export const registerUser = (userObj) => {
+export const setLoggedInUser = (userObj) => {
+    loggedInUser = userObj
+}
+
+//fetch call to login a user
+export const loginUser = (userObj) => {
+    return fetch(`http://localhost:8088/users?name=${userObj.name}&email=${userObj.email}`)
+        .then(response => response.json())
+        .then(parsedUser => {
+            //is there a user?
+            console.log("parsedUser", parsedUser) //data is returned as an array
+            if (parsedUser.length > 0) {
+                setLoggedInUser(parsedUser[0]);
+                return getLoggedInUser();
+            } else {
+                //no user
+                return false;
+            }
+        })
+}
+
+//fetch call to add a new user
+export const registerUser = (userObj) => {
     return fetch(`http://localhost:8088/users`, {
-      method: "POST",
+        method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(userObj)
     })
-    .then(response => response.json())
-    .then(parsedUser => {
-      setLoggedInUser(parsedUser);
-      return getLoggedInUser();
-    })
-  }
+        .then(response => response.json())
+        .then(parsedUser => {
+            setLoggedInUser(parsedUser);
+            return getLoggedInUser();
+        })
+}
--- src/scripts/feed/Post.js
+++ src/scripts/feed/Post.js
@@ -1,14 +1,29 @@
+import { getLoggedInUser } from "../data/Datamanager.js"
+
 export const Post = (postObject) => {
-    return `
-      <section class="post">
-        <header>
-            <h2 class="post__title">${postObject.title}</h2>
-        </header>
-        <img class="post__image" src="${postObject.imageURL}" />
-        <p>${postObject.description}</p>
-        <p>${postObject.user.name} Grammed at ${postObject.timestamp}</p>
-        <button id="edit__${postObject.id}">Edit</button>
-        <button id="delete__${postObject.id}">Delete</button>
-      </section>
-    `
+    if (postObject.user.id === getLoggedInUser().id) {
+      return `
+          <section class="post">
+            <header>
+                <h2 class="post__title">${postObject.title}</h2>
+            </header>
+            <img class="post__image" src="${postObject.imageURL}" />
+            <p>${postObject.description}</p>
+            <p>${postObject.user.name} Grammed at ${postObject.timestamp}</p>
+            <button id="edit__${postObject.id}">Edit</button>
+            <button id="delete__${postObject.id}">Delete</button>
+          </section>
+        `
+    }else {
+      return `
+          <section class="post">
+            <header>
+                <h2 class="post__title">${postObject.title}</h2>
+            </header>
+            <img class="post__image" src="${postObject.imageURL}" />
+            <p>${postObject.description}</p>
+            <p>${postObject.user.name} Grammed at ${postObject.timestamp}</p>
+          </section>
+        `
+    }
   }
