80fe25e5abee8b43c14304cbece633c3924bb4da
bvinson23 <brandonvinson23@gmail.com>
2021-03-24 11:13:40 -0500

    added a way for users to login, register, and logout

--- src/scripts/data/Datamanager.js
+++ src/scripts/data/Datamanager.js
@@ -10,14 +10,17 @@ export const usePostCollection = () => {
     // The spread operator makes this quick work
      return [...postCollection]
 }
+
 export const getPosts = () => {
-    return fetch("http://localhost:8088/posts")
-    .then(response => response.json())
-    .then(parsedResponse => {
+    const userId = getLoggedInUser().id
+    return fetch(`http://localhost:8088/posts?_expand=user`)
+      .then(response => response.json())
+      .then(parsedResponse => {
+        console.log("data with user", parsedResponse)
         postCollection = parsedResponse
         return parsedResponse;
-    })
-}
+      })
+  }
 
 export const createPost = postObj => {
     return fetch("http://localhost:8088/posts", {
@@ -72,4 +75,35 @@ export const deletePost = postId => {
 
   export const setLoggedInUser = (userObj) => {
       loggedInUser = userObj
+  }
+
+  export const loginUser = (userObj) => {
+    return fetch(`http://localhost:8088/users?name=${userObj.name}&email=${userObj.email}`)
+    .then(response => response.json())
+    .then(parsedUser => {
+      //is there a user?
+      console.log("parsedUser", parsedUser) //data is returned as an array
+      if (parsedUser.length > 0){
+        setLoggedInUser(parsedUser[0]);
+        return getLoggedInUser();
+      }else {
+        //no user
+        return false;
+      }
+    })
+  }
+
+  export const registerUser = (userObj) => {
+    return fetch(`http://localhost:8088/users`, {
+      method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userObj)
+    })
+    .then(response => response.json())
+    .then(parsedUser => {
+      setLoggedInUser(parsedUser);
+      return getLoggedInUser();
+    })
   }
